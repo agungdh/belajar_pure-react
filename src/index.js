ac7f275d73bef1ef05e310d350e6f44b60fdfc2a
agungdh <agungdh@live.com>
2018-12-08 10:27:42 +0700

    error box

--- src/index.js
+++ src/index.js
@@ -2,40 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types'
 
-function Email({data}) {
+function ErrorBox({children}) {
     return (
         <div>
-            <table border="1">
-                <tbody>
-                    <tr>
-                        <td>{data.sender}</td>
-                        <td>{data.subject}</td>
-                        <td>{data.date}</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="3">{data.message}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <i className="fas fa-exclamation-triangle"/>
+            {children}
         </div>
     )
+}
+
+ErrorBox.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.string
+    ]).isRequired
 };
 
-Email.propTypes = {
-    data: PropTypes.shape({
-        sender: PropTypes.string.isRequired,
-        subject: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-        message: PropTypes.string.isRequired
-    }).isRequired
-};
-
-var emailData = {
-    sender: "AgungDH",
-    subject: "Entah apa ini",
-    date: "Jun 14",
-    message: "Ini apa yaaa... kok aku paok banget -_-"
-};
-
-ReactDOM.render(<Email data={emailData}/>,
-    document.querySelector("#root"));
+ReactDOM.render(
+    <ErrorBox>The World Is Ending !!!</ErrorBox>,
+    document.querySelector('#root')
+);
