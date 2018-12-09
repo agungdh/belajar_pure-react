58c43073bf1927f12553b5e642c5aa964f08cba5
agungdh <agungdh@live.com>
2018-12-09 13:12:00 +0700

    dialog

--- src/index.js
+++ src/index.js
@@ -2,136 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types'
 
-function Tail({children, count}) {
-    let items = React.Children.toArray(children);
-
-    let toReturn = [];
-    for (let i = count; i >= 1; i--) {
-        toReturn.push(items[i - 1]);
-    }
-
-    return toReturn;
-}
-
-Tail.propTypes = {
-    children: PropTypes.node.isRequired,
-    count: PropTypes.number.isRequired
-}
-
-var tailCount= 3;
-
-function Head({children, count}) {
-    let items = React.Children.toArray(children);
-
-    let toReturn = [];
-    for (let i = 1; i <= count; i++) {
-        toReturn.push(items[i - 1]);
-    }
-
-    return toReturn;
-}
-
-Head.propTypes = {
-    children: PropTypes.node.isRequired,
-    count: PropTypes.number.isRequired
-}
-
-var headCount = 4;
-
-function LastChildOnly({children}) {
-    let items = React.Children.toArray(children);
-
-    return items[items.length - 1];
-}
-
-LastChildOnly.propTypes = {
-    children: PropTypes.node.isRequired
-};
-
-function AllChild({children}) {
-    return children;
-}
-
-AllChild.propTypes = {
-    children: PropTypes.node.isRequired
-};
-
-function FirstChildOnly({children}) {
-    let items = React.Children.toArray(children);
-
-    return items[0];
-}
-
-FirstChildOnly.propTypes = {
-    children: PropTypes.node.isRequired
-};
-
-function TestAja() {
+function Separator() {
     return (
-        <div>
-            <p>Test Aja</p>
-            <table border="1">
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                </tr><tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                </tr><tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+        <div><hr/></div>
     )
 }
 
-function TestAjaLagi() {
+function Dialog({children}) {
+    let items = React.Children.toArray(children);
+    let toReturn = [];
+
+    for (let i = 0; i <= items.length - 1; i++) {
+        toReturn.push(items[i]);
+
+        // if (i -1 != items.length) {
+            toReturn.push(<Separator/>);
+        // }
+    }
+    
     return (
-        <div>
-            <p>Test Aja Lagi</p>
-            <table border="1">
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                </tr><tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                </tr><tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+        <div>{toReturn}</div>
+    )
+}
+
+function Title({children}) {
+    return (
+        <div>{children}</div>
+    )
+}
+
+Title.propTypes = {
+    children: PropTypes.string.isRequired
+}
+
+function Body({children}) {
+    return (
+        <div>{children}</div>
+    )
+}
+
+Body.propTypes = {
+    children: PropTypes.string.isRequired
+}
+
+function Footer({children}) {
+    return (
+        <div>{children}</div>
     )
 }
 
 ReactDOM.render(
-    <Tail count={tailCount}>
-        <TestAja/>
-        <TestAjaLagi/>
-        <TestAja/>
-        <TestAjaLagi/>
-        <TestAja/>
-        <TestAjaLagi/>
-        <TestAja/>
-        <TestAjaLagi/>
-    </Tail>,
+    <Dialog>
+        <Title>Judul</Title>
+        <Body>Badan</Body>
+        <Footer>Kaki</Footer>
+    </Dialog>,
     document.querySelector('#root')
 );
